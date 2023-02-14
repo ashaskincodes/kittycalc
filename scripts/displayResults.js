f8a2b01c240436f9945c1417c1a98bfd7c9c6183
ashaskincodes <ashaskincodes@gmail.com>
2023-02-14 14:41:01 +0000

    ready for styling

--- scripts/displayResults.js
+++ scripts/displayResults.js
@@ -1,7 +1,18 @@
 function displayResults() {
     const newDiv = document.createElement("div");
+    newDiv.setAttribute('id','answer');
     const newContent = document.createTextNode(`Your cat is ${Math.round(humanAge)} in human years!`);
     newDiv.appendChild(newContent);
     const currentDiv = document.getElementById("calculate");
     document.body.insertBefore(newDiv, currentDiv.nextSibling);
+}
+
+function confirmResults() {
+    if(document.getElementById("answer")){
+        const element = document.getElementById('answer');
+        element.remove();
+        displayResults();
+    } else {
+        displayResults();
+    }
 }
