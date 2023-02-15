6c3c5296019a4d482fff34730977950f12c8b749
ashaskincodes <ashaskincodes@gmail.com>
2023-02-15 11:27:24 +0000

    logo added

--- scripts/displayResults.js
+++ scripts/displayResults.js
@@ -1,7 +1,7 @@
 function displayResults() {
     const newDiv = document.createElement("div");
     newDiv.setAttribute('id','answer');
-    const newContent = document.createTextNode(`Your cat is ${Math.round(humanAge)} in human years!`);
+    const newContent = document.createTextNode(`Your cat is ${Math.round(humanAge)} in human years.`);
     newDiv.appendChild(newContent);
     const currentDiv = document.getElementById("form");
     document.body.insertBefore(newDiv, currentDiv.nextSibling);
